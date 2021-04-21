c196dc11aa856667b25fb50d647f5df82aaea7ea
Benson <54143712+bensonkar@users.noreply.github.com>
2021-04-22 00:52:20 +0300

    first commit

---
+++ src/components/users/usersStore.js
@@ -0,0 +1,58 @@
+import { makeAutoObservable, runInAction } from "mobx";
+import axios from "axios";
+
+class usersStore {
+  users = [];
+  singleUser = [];
+  userIsLoading = false;
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  // Fetch user data
+
+  async getUserInfo() {
+    this.userIsLoading = true;
+    const userData = await axios
+      .get("https://607e868602a23c0017e8b79e.mockapi.io/api/v1/users")
+      .then((response) => {
+        this.users = response.data;
+        runInAction(() => {
+          this.userIsLoading = false;
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+  async getUserInfoById(id) {
+    this.userIsLoading = true;
+    const userData = await axios
+      .get(`https://607e868602a23c0017e8b79e.mockapi.io/api/v1/users/${id}`)
+      .then((response) => {
+        this.singleUser = response.data;
+        runInAction(() => {
+          this.userIsLoading = false;
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  async editUserInfo(id, data) {
+    this.userIsLoading = true;
+    await axios
+      .put(
+        `https://607e868602a23c0017e8b79e.mockapi.io/api/v1/users/${id}`,
+        data
+      )
+      .catch((error) => {
+        runInAction(() => {
+          this.userIsLoading = false;
+        });
+      });
+    runInAction(() => {
+      this.userIsLoading = false;
+    });
+  }
+}
+const userStore = new usersStore();
+
+export default userStore;
